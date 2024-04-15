0e57a2f893bf978c9307edf4fdf3ca0be4d13dea
alireza_sariri <alirezasariri78@gmail.com>
2024-04-15 19:12:55 +0330

    create FindCapitalByProvince Module

---
+++ PersianTools/Modules/FindCapitalByProvince.cs
@@ -0,0 +1,18 @@
+﻿using PersianTools.Modules.capital_by_province;
+
+namespace PersianTools.Modules
+{
+    public static class FindCapitalByProvince
+    {
+        public static string Find(string province)
+        {
+            if (string.IsNullOrWhiteSpace(province))
+            {
+                return string.Empty;
+            }
+            return State.CapitalProvince.ContainsKey(province)
+                ? State.CapitalProvince[province] 
+                : string.Empty;
+        }
+    }
+}
---
+++ PersianToolsTests/FindCapitalByProvinceTest.cs
@@ -0,0 +1,45 @@
+﻿using PersianTools.Modules;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+
+namespace PersianToolsTests
+{
+
+
+    [ExcludeFromCodeCoverage]
+    public class FindCapitalByProvinceTest
+    {
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("", "")]
+        [InlineData(" ", "")]
+
+        public void Null_Empty_Input_Test(string input, string expected)
+        {
+            Assert.Equal(expected, FindCapitalByProvince.Find(input));
+        }
+
+
+        [Theory]
+        [InlineData("قم", "قم")]
+        [InlineData("چهارمحال و بختیاری", "شهرکرد")]
+
+        public void Check_Exists_Province(string input, string expected)
+        {
+            Assert.Equal(expected, FindCapitalByProvince.Find(input));
+        }
+
+
+        [Theory]
+        [InlineData("لندن", "")]
+        [InlineData("London", "")]
+        public void Check_None_Exists_Province(string input, string expected)
+        {
+            Assert.Equal(expected, FindCapitalByProvince.Find(input));
+        }
+
+    }
+}
